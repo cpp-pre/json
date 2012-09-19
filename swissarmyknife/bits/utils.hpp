04a4e634d5a3908baaab31aa7bef20703f105bd0
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2012-09-19 11:19:27 +0200

    FEATURE: First commit of the swissarmyknife library with utils for bytes and bits.

---
+++ swissarmyknife/bits/utils.hpp
@@ -0,0 +1,107 @@
+#ifndef SWISSARMYKNIFE_BITS_UTILS_HPP
+#define SWISSARMYKNIFE_BITS_UTILS_HPP
+
+#include <iostream>
+#include <bitset>
+#include <string>
+#include <sstream>
+#include <math.h>
+#include <boost/assert.hpp>
+#include <boost/cstdint.hpp>
+#include <boost/dynamic_bitset.hpp>
+#include <boost/container/vector.hpp>
+
+namespace swissarmyknife { namespace bits {
+
+    /**
+     * \brief Create string representation of the dynamic bitset given
+     * \param bitset
+     */
+    inline std::string to_string(const boost::dynamic_bitset<boost::uint8_t> & bitset) {
+        std::string result;
+	    boost::to_string(bitset, result);
+        return result;
+    }
+
+    /**
+     * \brief Returns a string representation in bits of the int given.
+     * \param value
+     * \return The string representing the int given
+     */
+    template <typename uintX_t>
+    inline std::string to_binstring(uintX_t value) {
+        std::stringstream bitsRepresentation;
+        const int shift = 8 * sizeof( uintX_t ) - 1;
+        uintX_t one = 1;
+        const uintX_t mask = one << shift;
+
+        for ( size_t i = 1; i <= shift + 1; i++ ) {
+          bitsRepresentation << ( value & mask ? '1' : '0' );
+          value <<= 1;
+        }
+
+       return bitsRepresentation.str();
+    }
+
+    template <typename uintX_t, size_t bitsetSize>
+    inline uintX_t to_uint(const std::bitset<bitsetSize> value) {
+        const size_t uintX_t_size = 8 * sizeof ( uintX_t ); 
+        
+        BOOST_ASSERT(uintX_t_size >= bitsetSize);
+
+        const uintX_t one = 1;
+        const uintX_t zero = 0;
+        uintX_t result = 0x0;
+        
+        for (size_t i=0; i < value.size(); ++i) {
+            result |= (value.test(i)) ? (one << i) : (zero << i);
+        }
+
+        return result;
+    }
+
+    /**
+     * \brief This function pads the given client bitset in the host by making a copy of clientBitset, 
+     * resizing the clientBitset or-ing it in the hostBitset and returning the hostBitset
+     * \param hostBitset Which will get the clientBitset or-ed in it (i.e. The hostBitset get shifted to the left of the size of the clientBitset) hostBitset should be bigger than clientBitset
+     * \param clientBitset The bitset which will be integrated in the host bitset
+     * \return The hostBitset
+     */
+    inline boost::dynamic_bitset<boost::uint8_t> & shift_in_bitset(boost::dynamic_bitset<boost::uint8_t> &hostBitset, const boost::dynamic_bitset<boost::uint8_t> & clientBitset) {
+        boost::dynamic_bitset<boost::uint8_t> clientBitsetCopy = clientBitset; 
+        clientBitsetCopy.resize(hostBitset.size()); // Resize to have the hostBitset size
+	  
+        hostBitset <<= clientBitset.size();
+        hostBitset |= clientBitsetCopy;
+	
+        return hostBitset;
+    }
+
+    /**
+     * \brief This converts the given boost::dynamic_bitset to an array of 8-bit wide bytes.
+     * \param bitset to convert
+     * \return The vector of bytes resulting of the conversion in fixed byte width
+     */
+    inline boost::container::vector<boost::uint8_t> to_bytearray(const boost::dynamic_bitset<boost::uint8_t> &bitset) {
+        size_t sizeBytesPadded = static_cast<size_t>(ceil(static_cast<double>(bitset.size()) / CHAR_BIT));
+        boost::container::vector<boost::uint8_t> bytes(sizeBytesPadded);
+        boost::container::vector<boost::uint8_t>::reverse_iterator iter = bytes.rbegin();
+        boost::to_block_range(bitset, iter);
+	
+        return bytes;
+    }
+
+    /**
+     * \brief Converts a byte array into a bitset
+     * \param byteArray
+     * \return The bitset corresponding to the given byte array
+     */
+    inline boost::dynamic_bitset<boost::uint8_t> to_bitset(const boost::container::vector<boost::uint8_t> &byteArray, size_t resultingBitSetSize = 0) {
+        boost::dynamic_bitset<boost::uint8_t> bitset((resultingBitSetSize == 0) ? (byteArray.size() * CHAR_BIT) : resultingBitSetSize);
+        boost::from_block_range(byteArray.rbegin(), byteArray.rend(), bitset);
+        
+        return bitset;
+    }
+}}
+
+#endif
---
+++ swissarmyknife/bytes/utils.hpp
@@ -0,0 +1,191 @@
+#ifndef SWISSARMYKNIFE_BYTES_UTILS_HPP
+#define SWISSARMYKNIFE_BYTES_UTILS_HPP
+
+#include <string>
+#include <sstream>
+#include <cctype>
+#include <math.h>
+#include <stdexcept>
+#include <fstream>
+#include <boost/cstdint.hpp>
+#include <boost/container/vector.hpp>
+#include <boost/container/map.hpp>
+#include <boost/assign.hpp>
+#include <boost/foreach.hpp>
+
+namespace swissarmyknife { namespace bytes {
+
+    /**
+     * \brief Constant telling how much bits there is in a nibble
+     */
+    const size_t NIBBLE_BITS = 4;
+
+    /**
+     * \brief Constant telling how much Nibbles are in a byte
+     */
+    const size_t BYTE_NIBBLES = 2;
+
+    /**
+     * \brief Convers the given byte array of the given length to a corresponding hexadecimal number in a string.
+     * \param bytes Array of bytes
+     * \param length Length of the byte array
+     */
+    inline std::string to_hexstring(const boost::uint8_t *bytes, const size_t length) {
+        uint8_t ch = 0x00;
+        size_t i = 0; 
+
+        if (bytes == NULL || length <= 0)
+            return std::string("");
+
+        /**
+         * Constant array which can be used to convert hex strings to real nibbles.
+         */
+        const char nibbleToChar[] = {'0', '1', '2', '3', '4', '5',
+                                     '6', '7', '8', '9', 'a', 'b',
+                                     'c', 'd', 'e', 'f'};
+	
+        std::string out;
+        out.reserve(length*2);
+        while (i < length) {
+	    
+            ch = (bytes[i] & 0xF0); // Strip off high nibble
+            ch = (ch >> NIBBLE_BITS); // shift the bits down
+            ch = (ch & 0x0F); // must do this is high order bit is on!
+	    
+            out.push_back(nibbleToChar[ch]); // convert the nibble to a String Character
+
+            ch = (bytes[i] & 0x0F); // Strip off low nibble 
+	    
+            out.push_back(nibbleToChar[ch]); // convert the nibble to a String Character
+	    
+            i++;
+        }
+
+        return out;
+    }
+
+    /**
+     * \brief Convers the given byte array of the given length to a corresponding binary number in a string.
+     * \param byteArray Array of bytes
+     */
+    inline std::string to_binstring(const boost::container::vector<boost::uint8_t> &byteArray) {
+        std::stringstream bitsRepresentation;
+        const int shift = 8 * sizeof( uint8_t ) - 1;
+        const uint8_t mask = 1 << shift;
+
+        BOOST_FOREACH(uint8_t byte, byteArray) {
+
+            for ( size_t i = 1; i <= shift + 1; i++ ) {
+                bitsRepresentation << ( byte & mask ? '1' : '0' );
+                byte <<= 1;
+            }
+        }
+
+        return bitsRepresentation.str();
+    }
+
+    /**
+     * \brief Converts the given hexChars to the byte corresponding
+     * \param highNibbleChar
+     * \param lowNibbleChar
+     */
+    inline boost::uint8_t from_hexchar(char highNibbleChar, char lowNibbleChar) {
+        const boost::container::map<char, boost::uint8_t> charToNibble = boost::assign::map_list_of('0', 0)('1', 1)('2', 2)
+                                                                                                    ('3', 3)('4', 4)('5', 5)
+                                                                                                    ('6', 6)('7', 7)('8', 8)
+                                                                                                    ('9', 9)('a', 10)('b', 11)
+                                                                                                    ('c', 12)('d', 13)('e', 14)
+                                                                                                    ('f', 15);
+        if (!isdigit(highNibbleChar)) {
+            highNibbleChar = tolower(highNibbleChar);
+        }
+
+        if (!isdigit(lowNibbleChar)) {
+            lowNibbleChar = tolower(lowNibbleChar);
+        }
+	
+        if ( (charToNibble.find(highNibbleChar) != charToNibble.end()) && ( charToNibble.find(lowNibbleChar) != charToNibble.end() ) ) {
+            boost::uint8_t highNibble = charToNibble.find(highNibbleChar)->second;
+            boost::uint8_t lowNibble = charToNibble.find(lowNibbleChar)->second;
+
+            highNibble <<= NIBBLE_BITS; // make it high
+            lowNibble |= highNibble; // merge them together
+        
+            return lowNibble;
+        }
+
+        return 0; // This is invalid
+    }
+
+    /**
+     * \brief This function can be used to parse a string of hexadecimal number back into bits representation. 
+     * Note that it runs from left to right, putting eventually the last nibble empty if the size of the input string isn't a multiple of 2.
+     * \param hexString to convert
+     */
+    inline boost::container::vector<boost::uint8_t> from_hexstring(const std::string &hexString) {
+        size_t nibblePaddedSize = ( (hexString.size() / BYTE_NIBBLES) + (hexString.size() % BYTE_NIBBLES) );
+        
+        if ((nibblePaddedSize & 1) != 0) {
+            ++nibblePaddedSize;
+        }
+
+        boost::container::vector<boost::uint8_t> result;
+        result.reserve(nibblePaddedSize);
+        
+        for (std::string::const_iterator iter = hexString.begin(); iter != hexString.end(); iter += BYTE_NIBBLES) {
+	    
+            if ( (iter+1) != hexString.end() ) {
+                result.push_back(from_hexchar(*iter, *(iter+1)));
+            } else {
+                result.push_back(from_hexchar(*iter, '0'));
+                break;
+            }
+        }
+        
+        return result;
+    }
+
+    /**
+     * \brief This function transforms any byteArray in the chosen (u)int*_t type.
+     * \param byteArray To transform into native uint
+     * \return The int requested with the value of the byteArray.
+     */
+    template<typename intX_t>
+    inline intX_t from_byteArray(const boost::container::vector<boost::uint8_t> &byteArray) {
+        intX_t result = 0x0;
+
+        for (size_t i = 0; i < sizeof(intX_t); ++i) {
+            result |= static_cast<intX_t>(byteArray[i] << (CHAR_BIT * (sizeof(intX_t) - (i+1))));
+        }
+
+        return result;
+    }
+    
+    /**
+     * \brief This methods opens up a file and parses it's bytes into a bytearray.
+     * \param binaryFilePath Where to find the file to load
+     * \return The bytearray corresponding to the file content
+     *
+     * \throws std::runtime_error In case the file isn't found.
+     */
+    inline boost::shared_ptr<boost::container::vector<boost::uint8_t> > load_bytearray(const std::string binaryFilePath) {
+        boost::shared_ptr<boost::container::vector<boost::uint8_t> > bytearray;
+        std::ifstream in(binaryFilePath.data(), std::ios_base::in | std::ios::binary);
+        
+        if (in.good()) {
+            in.seekg(0, in.end);
+            std::streampos lengthToRead = in.tellg();
+            in.seekg(0, in.beg);
+            
+            bytearray.reset(new boost::container::vector<boost::uint8_t>(lengthToRead, 0x0));
+            in.read(reinterpret_cast<char*>(&bytearray->at(0)), lengthToRead);
+            in.close();
+        } else {
+            throw std::runtime_error("File " + binaryFilePath + " not found.");
+        }
+
+        return bytearray;
+    }
+}}
+
+#endif
