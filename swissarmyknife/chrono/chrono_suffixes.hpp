337b0c6d53660bd58599c0294d7c5f3ecf3ce5b8
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2014-07-30 11:42:13 +0200

    FEATURE: Added boost::chrono suffixes until c++1y brings them to us.

---
+++ swissarmyknife/chrono/chrono_suffixes.hpp
@@ -0,0 +1,25 @@
+#ifndef SWISSARMYKNIFE_CHRONO_CHRONOSUFFIXES_HPP
+#define SWISSARMYKNIFE_CHRONO_CHRONOSUFFIXES_HPP
+
+#include <boost/chrono.hpp>
+
+namespace swissarmyknife { namespace chrono {
+
+  constexpr boost::chrono::milliseconds operator "" _ms(
+      unsigned long long ms) {
+    return boost::chrono::milliseconds(ms);
+  }
+
+  constexpr boost::chrono::seconds operator "" _sec(
+      unsigned long long sec) {
+    return boost::chrono::seconds(sec);
+  }
+
+  constexpr boost::chrono::milliseconds operator "" _min(
+      unsigned long long min) {
+    return boost::chrono::minutes(min);
+  }
+
+}}
+
+#endif
