a7038c55752c6670097e65e45277ffa97fe14abd
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2014-08-12 15:47:29 +0200

    FEATURE: Comparison operators for Boost.Ref.

---
+++ swissarmyknife/boost/ref/comparison.hpp
@@ -0,0 +1,45 @@
+/**
+ * \brief Provides comparison operators for Boost.Ref reference_wrapper.
+ */
+#ifndef SWISSARMYKNIFE_BOOST_REF_COMPARISON_HPP
+#define SWISSARMYKNIFE_BOOST_REF_COMPARISON_HPP
+
+#include <swissarmyknife/detail/config.hpp>
+
+#include <boost/ref.hpp>
+
+namespace boost {
+
+  template<typename T>
+  inline bool operator==(const reference_wrapper<T>& lhs, const reference_wrapper<T>& rhs) {
+    return lhs.get() == rhs.get();
+  }
+
+  template<typename T>
+  inline bool operator< (const reference_wrapper<T>& lhs, const reference_wrapper<T>& rhs) {
+    return lhs.get() < rhs.get();
+  }
+
+  template<typename T>
+  inline bool operator!=(const reference_wrapper<T>& lhs, const reference_wrapper<T>& rhs){
+    return lhs.get() != rhs.get();
+  }
+
+  template<typename T>
+  inline bool operator> (const reference_wrapper<T>& lhs, const reference_wrapper<T>& rhs){
+    return lhs.get() > rhs.get();
+  } 
+
+  template<typename T>
+  inline bool operator<=(const reference_wrapper<T>& lhs, const reference_wrapper<T>& rhs){
+    return lhs.get() <= rhs.get();
+  } 
+
+  template<typename T>
+  inline bool operator>=(const reference_wrapper<T>& lhs, const reference_wrapper<T>& rhs){
+    return lhs.get() >= rhs.get();
+  }
+
+}
+
+#endif
