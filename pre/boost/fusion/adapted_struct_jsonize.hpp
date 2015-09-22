9640e9ecc6e40995290b4e001ac9dce56318b91d
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2015-09-22 22:50:59 +0200

    FEATURE: Capacity to dump to json any adapted struct. Naive, first version.

--- pre/boost/fusion/adapted_struct_jsonize.hpp
+++ pre/boost/fusion/adapted_struct_jsonize.hpp
@@ -1,31 +1,28 @@
-#ifndef BOOST_FUSION_ADAPTED_STRUCT_PRINTER_HPP
-#define BOOST_FUSION_ADAPTED_STRUCT_PRINTER_HPP
+#ifndef BOOST_FUSION_ADAPTED_STRUCT_JSONIZE_HPP
+#define BOOST_FUSION_ADAPTED_STRUCT_JSONIZE_HPP
 
 #include <boost/variant.hpp>
 #include <pre/boost/fusion/traits/is_boost_variant.hpp>
+#include <pre/boost/fusion/traits/is_container.hpp>
 #include <boost/fusion/include/tag_of.hpp>
+
 
 #include <boost/fusion/include/is_sequence.hpp>
 #include <pre/boost/fusion/for_each_member.hpp>
 
-#include <boost/fusion/include/io.hpp>
+#include <boost/fusion/include/for_each.hpp>
 #include <boost/fusion/include/boost_tuple.hpp>
-#include <pre/iostreams/indenting_ostream.hpp>
-#include <pre/boost/fusion/detail/cstdint_to_hexa_stream_operators.hpp>
-#include <pre/boost/fusion/detail/std_containers_ostream_operators.hpp>
 
+#include <nlohmann/json.hpp>
 
 namespace boost { namespace fusion {
 
 
   namespace detail {
-    using pre::iostreams::indent;
-    using pre::iostreams::deindent;
-
     template<class T>
     using enable_if_is_not_sequence_nor_variant_t = typename std::enable_if<
-      ! ( (boost::fusion::traits::is_sequence<T>::value )
-
+      ! ( boost::fusion::traits::is_container<T>::value || 
+          boost::fusion::traits::is_sequence<T>::value 
           || traits::is_boost_variant<T>::value)
     ,T>::type;
 
@@ -53,101 +50,124 @@ namespace boost { namespace fusion {
       traits::is_boost_variant<T>::value
     ,T>::type;
 
+    template<class T>
+    using enable_if_is_container_t = typename std::enable_if<
+      boost::fusion::traits::is_container<T>::value && !boost::fusion::traits::is_sequence<T>::value && !traits::is_boost_variant<T>::value
+    ,T>::type;
 
-    struct adapted_struct_printer : public boost::static_visitor<> {
+    struct adapted_struct_jsonize : public boost::static_visitor<> {
 
-      adapted_struct_printer(std::ostream& os) 
-        : boost::static_visitor<>(),  m_os(os) {}
+      adapted_struct_jsonize(nlohmann::json& json_object) 
+        : boost::static_visitor<>(),  _json_object(json_object) {}
 
       template<class T, 
         enable_if_is_adapted_struct_t<T>* = nullptr>
       void operator()(const char* name, const T& value) const {
-        m_os << name << " :";
-        m_os << indent;
-        this->operator()(value);
-        m_os << deindent;
+        nlohmann::json json_subobject;
+        adapted_struct_jsonize subjsonizer(json_subobject);
+        subjsonizer(value);
+
+        _json_object[name] = json_subobject;
       }
 
       template<class T, 
         enable_if_is_adapted_struct_t<T>* = nullptr>
       void operator()(const T& value) const {
-        m_os << "\n{" << indent << "\n";
         boost::fusion::for_each_member(value, *this);
-        m_os << deindent << "}\n";
       }
 
       template<class T, 
         enable_if_is_other_sequence_and_not_variant_t<T>* = nullptr>
       void operator()(const char* name, const T& value) const {
-        m_os << name << " :";
-        m_os << indent;
-        this->operator()(value);
-        m_os << deindent;
+        nlohmann::json json_subobject;
+        adapted_struct_jsonize subjsonizer(json_subobject);
+        subjsonizer(value);
+
+        _json_object[name] = json_subobject;
       }
 
       template<class T, 
         enable_if_is_other_sequence_and_not_variant_t<T>* = nullptr>
       void operator()(const T& value) const {
-        m_os << "\n{" << indent << "\n";
-        boost::fusion::operator<<(m_os, value);
-        m_os << "\n" << deindent << "}\n";
+        boost::fusion::for_each(value, *this);
+        // HMMMMM ???
       }
 
       template<class T, 
         enable_if_is_variant_t<T>* = nullptr>
       void operator()(const char* name, const T& value) const {
-        m_os << name << " : \n";
-        m_os << indent;
-        this->operator()(value);
-        m_os << deindent;
+        nlohmann::json json_subobject;
+        adapted_struct_jsonize subjsonizer(json_subobject);
+        subjsonizer(value);
+
+        _json_object[name] = json_subobject;
       }
 
       template<class T, 
         enable_if_is_variant_t<T>* = nullptr>
       void operator()(const T& value) const {
-        m_os << "\n{" << indent << "\n";
         boost::apply_visitor(*this, value);
-        m_os << deindent << "}\n";
       }
 
       template<class T, 
         enable_if_is_not_sequence_nor_variant_t<T>* = nullptr>
       void operator()(const char* name, const T& value) const {
-        m_os << name << " : ";
-        this->operator()(value);
-        m_os << ",\n";
+        nlohmann::json json_subobject;
+        adapted_struct_jsonize subjsonizer(json_subobject);
+        subjsonizer(value);
+
+        _json_object[name] = json_subobject;
       }
 
       template<class T, 
         enable_if_is_not_sequence_nor_variant_t<T>* = nullptr>
       void operator()(const T& value) const {
-        using boost::fusion::operator<<;
-        m_os << value;
+        _json_object = value;
+      }
+
+      template<class T, 
+        enable_if_is_container_t<T>* = nullptr>
+      void operator()(const char* name, const T& value) const {
+        nlohmann::json json_subobject;
+        adapted_struct_jsonize subjsonizer(json_subobject);
+        subjsonizer(value);
+
+        _json_object[name] = json_subobject;
+      }
+
+      template<class T, 
+        enable_if_is_container_t<T>* = nullptr>
+      void operator()(const T& value) const {
+        for (const auto& each : value) {
+          nlohmann::json json_subobject;
+          adapted_struct_jsonize subjsonizer(json_subobject);
+          subjsonizer(each);
+          _json_object.push_back(json_subobject); 
+        }
       }
       
       private:
-        std::ostream& m_os;
+      nlohmann::json& _json_object;
       
     };
 
   }
 
-  namespace adapted_struct_printer {
+  namespace adapted_struct_jsonize {
     template<class T, 
       detail::enable_if_is_adapted_struct_t<T>* = nullptr>
-    inline std::ostream& operator<<(std::ostream& os, const T& value) {
-        pre::iostreams::indenting_stream indentos(os);
-        detail::adapted_struct_printer printer(indentos);
-        printer(value);
-        return os;
+    nlohmann::json jsonize(const T& value) {
+      nlohmann::json json_object;
+      detail::adapted_struct_jsonize jsonizer(json_object);
+      jsonizer(value);
+      return json_object;
     }
 
 
   }
 
-  using std_containers_ostream_operators::operator<<;
-
 
 }}
+
 
 #endif
---
+++ test/adapted_struct_jsonize.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include <vector>
+#include <list>
+
+#include <boost/fusion/include/define_struct.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <pre/boost/fusion/adapted_struct_jsonize.hpp>
+#include <boost/variant.hpp>
+
+#include <boost/tuple/tuple.hpp>
+
+typedef size_t years;
+
+struct skill {
+  std::string skill_name;
+  years experience;
+};
+
+BOOST_FUSION_ADAPT_STRUCT(skill,
+    skill_name,
+    experience)
+
+BOOST_FUSION_DEFINE_STRUCT((datamodel), customer,
+    (std::string, name)
+    (size_t, age)
+    (std::vector<int>, friends_id)
+    (std::list<skill>, skillset)
+    )
+
+int main(int argc, char** argv) {
+  datamodel::customer instance {
+    "Mr. Dupond",
+    43,
+    {1,5},
+    {skill{"C++", 10}, skill{"GML", 20}, skill{"Linux", 2}}
+  };
+
+  auto json = boost::fusion::adapted_struct_jsonize::jsonize(instance);
+  
+  std::cout << json << std::endl;
+  return 0;
+}
