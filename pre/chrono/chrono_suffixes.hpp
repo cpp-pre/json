181aaa6966a164516188cc9044a2ed5ed9a29175
Patrick Wieder <patrick.wieder@ch.sauter-bc.com>
2016-05-02 18:41:11 +0200

    BUGFIX: violated ODR

--- pre/chrono/chrono_suffixes.hpp
+++ pre/chrono/chrono_suffixes.hpp
@@ -10,34 +10,34 @@ namespace pre { namespace chrono {
   
   namespace boost {
 
-    ::boost::chrono::milliseconds operator "" _ms(
+    inline ::boost::chrono::milliseconds operator "" _ms(
         unsigned long long ms) {
       return ::boost::chrono::milliseconds(ms);
     }
 
-    ::boost::chrono::seconds operator "" _sec(
+    inline ::boost::chrono::seconds operator "" _sec(
         unsigned long long sec) {
       return ::boost::chrono::seconds(sec);
     }
 
-    ::boost::chrono::milliseconds operator "" _min(
+    inline ::boost::chrono::milliseconds operator "" _min(
         unsigned long long min) {
       return ::boost::chrono::minutes(min);
     }
   }
 
   namespace std {
-    ::std::chrono::milliseconds operator "" _ms(
+    inline ::std::chrono::milliseconds operator "" _ms(
         unsigned long long ms) {
       return ::std::chrono::milliseconds(ms);
     }
 
-    ::std::chrono::seconds operator "" _sec(
+    inline ::std::chrono::seconds operator "" _sec(
         unsigned long long sec) {
       return ::std::chrono::seconds(sec);
     }
 
-    ::std::chrono::milliseconds operator "" _min(
+    inline ::std::chrono::milliseconds operator "" _min(
         unsigned long long min) {
       return ::std::chrono::minutes(min);
     }
