b1a14a2f7388ce14797dea11a3150d754d32c717
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2016-09-13 17:23:18 +0200

    BUGFIX: Added different const- non-const methods for apply_visitor.

--- pre/variant/apply_visitor.hpp
+++ pre/variant/apply_visitor.hpp
@@ -11,24 +11,36 @@
 
 namespace pre { namespace variant { 
 
-  // Const
+  // Boost.Variant
   template<class VisitorType, class VariantType,
     typename std::enable_if< pre::json::traits::is_boost_variant<VariantType>::value >::type* = nullptr>
   inline auto apply_visitor(const VisitorType& visitor, const VariantType& variant) -> decltype(boost::apply_visitor(visitor, variant)) {
     return variant.apply_visitor(visitor);
   }
 
+  template<class VisitorType, class VariantType,
+    typename std::enable_if< pre::json::traits::is_boost_variant<VariantType>::value >::type* = nullptr>
+  inline auto apply_visitor(const VisitorType& visitor, VariantType& variant) -> decltype(boost::apply_visitor(visitor, variant)) {
+    return variant.apply_visitor(visitor);
+  }
+
+  template<class VisitorType, class VariantType,
+    typename std::enable_if< pre::json::traits::is_boost_variant<VariantType>::value >::type* = nullptr>
+  inline auto apply_visitor(VisitorType& visitor, VariantType& variant) -> decltype(boost::apply_visitor(visitor, variant)) {
+    return variant.apply_visitor(visitor);
+  }
+
+  // Mapbox.Variant
   template<class VisitorType, class VariantType, 
     typename std::enable_if< mapbox::traits::is_mapbox_variant<VariantType>::value >::type* = nullptr>
   inline auto apply_visitor(const VisitorType& visitor, const VariantType& variant) -> decltype(mapbox::util::apply_visitor(visitor, variant)) {
     return mapbox::util::apply_visitor(visitor, variant);
   }
 
-  // Non-const
-  template<class VisitorType, class VariantType,
-    typename std::enable_if< pre::json::traits::is_boost_variant<VariantType>::value >::type* = nullptr>
-  inline auto apply_visitor(VisitorType& visitor, VariantType& variant) -> decltype(boost::apply_visitor(visitor, variant)) {
-    return variant.apply_visitor(visitor);
+  template<class VisitorType, class VariantType, 
+    typename std::enable_if< mapbox::traits::is_mapbox_variant<VariantType>::value >::type* = nullptr>
+  inline auto apply_visitor(const VisitorType& visitor, VariantType& variant) -> decltype(mapbox::util::apply_visitor(visitor, variant)) {
+    return mapbox::util::apply_visitor(visitor, variant);
   }
 
   template<class VisitorType, class VariantType, 
